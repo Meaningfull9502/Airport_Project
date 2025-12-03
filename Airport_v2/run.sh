48bf162ce4037922e082d0770c29fd7f0f9cda1e
junghoon9502 <55870707+Meaningfull9502@users.noreply.github.com>
2025-12-04 00:42:18 +0900

    Update run.sh

--- Airport_v2/run.sh
+++ Airport_v2/run.sh
@@ -1,9 +1,3 @@
-pip install pandas
-pip install matplotlib
-pip install scikit-learn
-pip install tqdm
-pip install reformer_pytorch
-
 #  Airport 데이터셋에 대해 시계열 예측을 수행하는 스크립트입니다.
 # nested cross validation 아래와 같이 실행
 ## train/val/test: 2002~2015/2016/2017, 2002~2016/2017/2018, 2002~2017/2018/2019 --> 평균 내어 최적 파라미터로 2002~2018/2019/2024~2025(코로나 2020~2023 제외) 데이터셋에 적용
