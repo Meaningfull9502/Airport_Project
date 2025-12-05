32ac50b1364e59a4a36f79efdeb625b98d5a4327
seunghan96 <seunghan96@naver.com>
2025-12-06 08:39:35 +0900

    results

--- Airport_final/run.sh
+++ Airport_final/run.sh
@@ -28,20 +28,13 @@
 
 # 학습
 # 공변량 사용
-cd ./Airport
-python run.py --target both --pred_len 56 --use_covar
-python run.py --target both --pred_len 14 --use_covar
+#python run.py --target both --pred_len 56 --use_covar --gpu 6
+python run.py --target both --pred_len 14 --use_covar --gpu 6
 
-python run.py --target arrival --pred_len 56 --use_covar
-python run.py --target arrival --pred_len 56 
-python run.py --target arrival --pred_len 14 --use_covar
-python run.py --target arrival --pred_len 14 
+python run.py --target arrival --pred_len 56 --use_covar --gpu 6
+python run.py --target arrival --pred_len 56 --gpu 6 
+python run.py --target arrival --pred_len 14 --use_covar --gpu 6
+python run.py --target arrival --pred_len 14 --gpu 6 
 
-python run.py --target departure --pred_len 56 --use_covar
-python run.py --target departure --pred_len 56 
-python run.py --target departure --pred_len 14 --use_covar
-python run.py --target departure --pred_len 14
-
-python run.py --target both --pred_len 56 --use_covar --use_dbloss
-python run.py --target arrival --pred_len 56 --use_covar --use_dbloss
-python run.py --target departure --pred_len 56 --use_covar --use_dbloss
+python run.py --target departure --pred_len 56 --use_covar --gpu 6
+python run.py --target departure --pred_len 56 --gpu 6 
