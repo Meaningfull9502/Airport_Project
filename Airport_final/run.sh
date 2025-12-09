365b2b7375f533aa51da0fc9676f28479b766c5f
junghoon9502 <55870707+Meaningfull9502@users.noreply.github.com>
2025-12-09 18:03:48 +0900

    Update run.sh

--- Airport_final/run.sh
+++ Airport_final/run.sh
@@ -33,8 +33,8 @@ python run.py --target both --pred_len 14 --use_covar --gpu 6
 
 python run.py --target arrival --pred_len 56 --use_covar --gpu 6
 python run.py --target arrival --pred_len 56 --gpu 6 
-python run.py --target arrival --pred_len 14 --use_covar --gpu 6
-python run.py --target arrival --pred_len 14 --gpu 6 
+#python run.py --target arrival --pred_len 14 --use_covar --gpu 6
+#python run.py --target arrival --pred_len 14 --gpu 6 
 
 python run.py --target departure --pred_len 56 --use_covar --gpu 6
 python run.py --target departure --pred_len 56 --gpu 6 
